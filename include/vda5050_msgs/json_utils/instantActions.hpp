fb0a638463a0469cdb63d1fbc748929131d5eb87
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-04 18:05:17 +0800

    feat: adds serializer and deserializer for ActionParameterVlaue message
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- include/vda5050_msgs/json_utils/instantActions.hpp
+++ include/vda5050_msgs/json_utils/instantActions.hpp
@@ -14,11 +14,46 @@ namespace msg {
     /// \param msg 
     void to_json(nlohmann::json& j, const ActionParameterValue& msg)
     {
-        to_json(j, msg.header);
-
         if (
-            
-        )
+            msg.type == ActionParameterValue::ARRAY ||
+            msg.type == ActionParameterValue::BOOL ||
+            msg.type == ActionParameterValue::NUMBER ||
+            msg.type == ActionParameterValue::STRING ||
+            msg.type == ActionParameterValue::OBJECT)
+        {
+            j["type"] = msg.type;
+        }
+        else
+        {
+            throw std::runtime_error("Serialization error: Unexpected type");
+        }
+        
+        j["value"] = msg.value;
+    }
+
+    /// \brief Populate a vda5050_msgs::msg::ActionParameterValue object from a nlohmann::json object
+    ///
+    /// \param j Reference to the JSON object containing serialized parameter value data
+    /// \param msg Reference to the ActionParameterValue message to populate    
+    ///
+    /// \throws std::runtime_error If failed to deserialize type 
+    void from_json(nlohmann::json& j, const ActionParameterValue& msg)
+    {
+        auto type = j.at("type").get<uint8_t>();
+        if (
+            type == ActionParameterValue::ARRAY ||
+            type == ActionParameterValue::BOOL ||
+            type == ActionParameterValue::NUMBER ||
+            type == ActionParameterValue::STRING ||
+            type == ActionParameterValue::OBJECT)
+        {
+            msg.type = type;
+        }
+        else
+        {
+            throw std::runtime_error("JSON parsing error: Unexpected type.");
+        }
+
     }
 
 } // namespace msg
