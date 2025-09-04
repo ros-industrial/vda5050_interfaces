1cf1c0d4ddbc8dabe4a2966b12f740e271163e86
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-04 17:17:56 +0800

    nit: adds header guards
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- include/vda5050_msgs/json_utils/instantActions.hpp
+++ include/vda5050_msgs/json_utils/instantActions.hpp
@@ -1,5 +1,7 @@
 #include <nlohmann/json.hpp>
 
+#include "vda5050_msgs/json_utils/header.hpp"
+
 #ifndef VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
 #define VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
 
@@ -7,7 +9,7 @@ namespace vda5050_msgs {
 
 namespace msg {
 
-    void to_json(nlohmann::json& j, const InstantActions& msg)
+    void to_json(nlohmann::json& j, const InstantAction& msg)
     {
         to_json(j, msg.header);
         j["actions"] = msg.actions;
