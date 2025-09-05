1a3e0013f287af99a790a6b353e079f5ea2fd90d
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-05 16:22:56 +0800

    fix: fixes serializer and deserializer function arguments to make the right object references const
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- include/vda5050_msgs/json_utils/instantAction.hpp
+++ include/vda5050_msgs/json_utils/instantAction.hpp
@@ -1,6 +1,8 @@
 #include <nlohmann/json.hpp>
 
 #include "vda5050_msgs/json_utils/header.hpp"
+#include "vda5050_msgs/msg/instant_action.hpp"
+
 
 #ifndef VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
 #define VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
@@ -38,7 +40,7 @@ namespace msg {
     /// \param msg Reference to the ActionParameterValue message to populate    
     ///
     /// \throws std::runtime_error If failed to deserialize type 
-    void from_json(nlohmann::json& j, const ActionParameterValue& msg)
+    void from_json(const nlohmann::json& j, ActionParameterValue& msg)
     {
         auto type = j.at("type").get<uint8_t>();
         if (
@@ -69,7 +71,7 @@ namespace msg {
     /// @brief populate a vda5050_msgs::msg::ActionParameter object from a nlohmann::json object
     /// @param j Reference to the JSON object containing serialized ActionParameter data
     /// @param msg Reference to the ActionParameter message to populate
-    void from_json(nlohmann::json& j, const ActionParameter& msg)
+    void from_json(const nlohmann::json& j, ActionParameter& msg)
     {
         auto key = j.at("key").get<std::string>();
         auto value = j.at("value").get<ActionParameterValue>();
@@ -113,7 +115,7 @@ namespace msg {
     /// @param j Reference to the JSON object containing serialized Action data
     /// @param msg Reference to the Action message to populate
     /// @throws std::runtime_error If failed to deserialize blockingType
-    void from_json(nlohmann::json& j, const Action& msg)
+    void from_json(const nlohmann::json& j, Action& msg)
     {
         auto action_type = j.at("actionType").get<std::string>();
         auto action_id = j.at("actionId").get<std::string>();
@@ -135,7 +137,7 @@ namespace msg {
         if (j.contains("actionDescription"))
         {
             auto action_description = j.at("actionDescription").get<std::string>();
-            msg.action_description = action_description;
+            msg.action_description.push_back(action_description);
         }
 
         if (j.contains("actionParameters"))
@@ -158,7 +160,7 @@ namespace msg {
     /// @brief populate a vda5050_msgs::msg::InstantAction object from a nlohmann::json object
     /// @param j Reference to the JSON object containing serialized InstantAction data
     /// @param msg Reference to the InstantAction object to be populated
-    void from_json(nlohmann::json& j, const InstantAction& msg)
+    void from_json(const nlohmann::json& j, InstantAction& msg)
     {
         from_json(j, msg.header);
         
