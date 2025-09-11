cb17aa1bb43070195b6f5887f5e080aa7d63940f
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-11 18:24:56 +0800

    fix: fixes field names for messages to follow snake case
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

---
+++ include/vda5050_msgs/json_utils/order.hpp
@@ -0,0 +1,7 @@
+#include <nlohmann/json.hpp>
+
+#include "vda5050_msgs/json_utils/header.hpp"
+// #include "vda5050_msgs/msg/order.hpp"
+
+#ifndef VDA5050_MSGS__JSON_UTILS__ORDER_HPP_
+#define VDA5050_MSGS__JSON_UTILS__ORDER_HPP_
