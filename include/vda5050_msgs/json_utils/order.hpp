cbcd916f4294394089407aff1b3172ea4bf0f335
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-12 15:50:10 +0800

    nit: adds comments for code structure clarity
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- include/vda5050_msgs/json_utils/order.hpp
+++ include/vda5050_msgs/json_utils/order.hpp
@@ -1,3 +1,6 @@
+#ifndef VDA5050_MSGS__JSON_UTILS__ORDER_HPP_
+#define VDA5050_MSGS__JSON_UTILS__ORDER_HPP_
+
 #include <nlohmann/json.hpp>
 
 #include <string>
@@ -5,9 +8,6 @@
 #include "vda5050_msgs/json_utils/header.hpp"
 #include "vda5050_msgs/json_utils/instantAction.hpp"
 #include "vda5050_msgs/msg/order.hpp"
-
-#ifndef VDA5050_MSGS__JSON_UTILS__ORDER_HPP_
-#define VDA5050_MSGS__JSON_UTILS__ORDER_HPP_
 
 namespace vda5050_msgs {
 
@@ -405,7 +405,7 @@ namespace msg {
             msg.zone_set_id.push_back(zone_set_id);
         }
     }
-}
-}
+} /// namespace msg
+} /// namespace vda5050_msgs
 
-#endif
+#endif /// VDA5050_MSGS__JSON_UTILS__ORDER_HPP_
