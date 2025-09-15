a57c26d9b50607b06441cbe55b431cf02b539bd1
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-15 17:11:05 +0800

    feat: creates a general schema validator for vda5050 messages
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

---
+++ include/vda5050_msgs/json_utils/schemas.hpp
@@ -0,0 +1,8 @@
+#ifndef VDA5050_MSGS__JSON_UTILS__SCHEMAS_HPP_
+#define VDA5050_MSGS__JSON_UTILS__SCHEMAS_HPP_
+
+#include <string>
+
+extern const char* connection_schema;
+
+#endif
---
+++ include/vda5050_msgs/json_utils/validators.hpp
@@ -0,0 +1,34 @@
+#include <nlohmann/json-schema.hpp>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include "include/vda5050_msgs/json_utils/schemas.hpp"
+
+/// @brief Checks that a JSON object is following the specified schema
+///
+/// @param schema The schema that the JSON object is to follow
+/// @param j Reference to the JSON object to be validated
+/// @return 
+int validate_schema(nlohmann::json schema, nlohmann::json& j)
+{
+    nlohmann::json_schema::json_validator validator;
+
+    try
+    {
+        validator.set_root_schema(schema);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Validation of schema failed: " << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    try
+    {
+        validator.validate(j);
+    }   
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return EXIT_SUCCESS;
+}
