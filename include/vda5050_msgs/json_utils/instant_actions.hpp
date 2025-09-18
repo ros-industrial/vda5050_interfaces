fd49e52d42484372a95da43eedb55212f713b2bb
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-18 16:29:10 +0800

    fix: renames instantAction.msg to instantActions.msg, renames instantAction.hpp to instant_actions.hpp
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- include/vda5050_msgs/json_utils/instant_actions.hpp
+++ include/vda5050_msgs/json_utils/instant_actions.hpp
@@ -1,7 +1,25 @@
+/*
+ * Copyright (C) 2025 ROS-Industrial Consortium Asia Pacific
+ * Advanced Remanufacturing and Technology Centre
+ * A*STAR Research Entities (Co. Registration No. 199702110H)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <nlohmann/json.hpp>
 
 #include "vda5050_msgs/json_utils/header.hpp"
-#include "vda5050_msgs/msg/instant_action.hpp"
+#include "vda5050_msgs/msg/instant_actions.hpp"
 
 
 #ifndef VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
@@ -150,7 +168,7 @@ namespace msg {
     /// @brief convert a vda5050_msgs::msg::InstantAction object to a nlohmann::json object
     /// @param j Reference to the JSON object to be populated
     /// @param msg Reference to the message object to serialize
-    void to_json(nlohmann::json& j, const InstantAction& msg)
+    void to_json(nlohmann::json& j, const InstantActions& msg)
     {
         to_json(j, msg.header);
 
@@ -160,7 +178,7 @@ namespace msg {
     /// @brief populate a vda5050_msgs::msg::InstantAction object from a nlohmann::json object
     /// @param j Reference to the JSON object containing serialized InstantAction data
     /// @param msg Reference to the InstantAction object to be populated
-    void from_json(const nlohmann::json& j, InstantAction& msg)
+    void from_json(const nlohmann::json& j, InstantActions& msg)
     {
         from_json(j, msg.header);
         
