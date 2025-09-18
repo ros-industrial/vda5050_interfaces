66ad1008c9f1f57edfd02c68df9657061d860bfd
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-18 14:29:38 +0800

    feat: adds enums for vda5050 objects
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- test/generator/jsonGenerator.hpp
+++ test/generator/jsonGenerator.hpp
@@ -1,14 +1,27 @@
 #ifndef TEST__GENERATOR__JSON_GENERATOR_HPP_
 #define TEST__GENERATOR__JSON_GENERATOR_HPP_
 
+#include <nlohmann/json.hpp>
+
 #include "generator.hpp"
 
+/// \brief Utility class to generate random VDA 5050 JSON objects 
 class RandomJSONgenerator
 {
 public:
 
+    /// \brief Generate a fully populated JSON object of a supported type 
+    nlohmann::json generate()
+    {
+
+    }
 
 private:
+    enum class JsonTypes {
+        Connection,
+        Order,
+        
+    }
 
 }
 
