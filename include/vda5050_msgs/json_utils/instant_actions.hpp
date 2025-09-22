548f3da915ef07dc6e5e88e7a8f87ebcb048d54b
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-22 10:35:16 +0800

    nit: adds newline between copyright comment and header guard
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- include/vda5050_msgs/json_utils/instant_actions.hpp
+++ include/vda5050_msgs/json_utils/instant_actions.hpp
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
 #define VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
 
