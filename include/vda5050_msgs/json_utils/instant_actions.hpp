549aba4bebe492f91b50cb154fcee75af484d87a
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-22 10:30:44 +0800

    fix: adds missing include files, uses direct assignment for deser/ser functions, adds missing deser field in actionparametervalue deser function
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- include/vda5050_msgs/json_utils/instant_actions.hpp
+++ include/vda5050_msgs/json_utils/instant_actions.hpp
@@ -19,6 +19,8 @@
 #define VDA5050_MSGS__JSON_UTILS__INSTANT_ACTIONS_HPP_
 
 #include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
 
 #include "vda5050_msgs/json_utils/header.hpp"
 #include "vda5050_msgs/msg/instant_actions.hpp"
@@ -73,6 +75,8 @@ void from_json(const nlohmann::json& j, ActionParameterValue& msg)
   {
     throw std::runtime_error("JSON parsing error: Unexpected type.");
   }
+
+  msg.value = j.at("value").get<std::string>();
 }
 
 /// \brief convert a vda5050_msgs::msg::ActionParameter object to a nlohmann::json object
@@ -91,11 +95,8 @@ void to_json(nlohmann::json& j, const ActionParameter& msg)
 /// \param msg Reference to the ActionParameter message to populate
 void from_json(const nlohmann::json& j, ActionParameter& msg)
 {
-  auto key = j.at("key").get<std::string>();
-  auto value = j.at("value").get<ActionParameterValue>();
-
-  msg.key = key;
-  msg.value = value;
+  msg.key = j.at("key").get<std::string>();
+  msg.value = j.at("value").get<ActionParameterValue>();
 }
 
 /// \brief convert a vda5050_msgs::msg::Action object to a nlohmann::json object
@@ -139,14 +140,10 @@ void to_json(nlohmann::json& j, const Action& msg)
 /// \throws std::runtime_error If failed to deserialize blockingType
 void from_json(const nlohmann::json& j, Action& msg)
 {
-  auto action_type = j.at("actionType").get<std::string>();
-  auto action_id = j.at("actionId").get<std::string>();
-
-  msg.action_type = action_type;
-  msg.action_id = action_id;
+  msg.action_type = j.at("actionType").get<std::string>();
+  msg.action_id = j.at("actionId").get<std::string>();
 
   auto blocking_type = j.at("blockingType").get<std::string>();
-
   if (
     blocking_type == Action::NONE || blocking_type == Action::SOFT ||
     blocking_type == Action::HARD)
@@ -160,8 +157,8 @@ void from_json(const nlohmann::json& j, Action& msg)
 
   if (j.contains("actionDescription"))
   {
-    auto action_description = j.at("actionDescription").get<std::string>();
-    msg.action_description.push_back(action_description);
+    msg.action_description.push_back(
+      j.at("actionDescription").get<std::string>());
   }
 
   if (j.contains("actionParameters"))
@@ -190,8 +187,7 @@ void from_json(const nlohmann::json& j, InstantActions& msg)
 {
   from_json(j, msg.header);
 
-  auto actions = j.at("actions").get<std::vector<Action>>();
-  msg.actions = actions;
+  msg.actions = j.at("actions").get<std::vector<Action>>();
 }
 
 }  // namespace msg
