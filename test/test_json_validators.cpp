827c2ca3a560d4e3716f192ffa434b119ad507af
Shawn Chan <shawnkengkiat@gmail.com>
2025-09-22 17:55:06 +0800

    nit: run linter
    Signed-off-by: Shawn Chan <shawnkengkiat@gmail.com>

--- test/test_json_validators.cpp
+++ test/test_json_validators.cpp
@@ -1,29 +1,31 @@
 #include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
 
-#include "vda5050_msgs/json_utils/validators.hpp"
-#include "vda5050_msgs/json_utils/schemas.hpp"
 #include "generator/jsonGenerator.hpp"
+#include "vda5050_msgs/json_utils/schemas.hpp"
+#include "vda5050_msgs/json_utils/validators.hpp"
 
-/// \brief Fixture class to create VDA5050 JSON objects for tests  
+/// \brief Fixture class to create VDA5050 JSON objects for tests
 class JsonValidatorTest : public ::testing::Test
 {
 protected:
-    JsonValidatorTest() 
-    {   
-        connection_object_json = json_generator.generate(RandomJSONgenerator::JsonTypes::Connection);
-        connection_schema_json = nlohmann::json::parse(connection_schema);
-    }
+  JsonValidatorTest()
+  {
+    connection_object_json =
+      json_generator.generate(RandomJSONgenerator::JsonTypes::Connection);
+    connection_schema_json = nlohmann::json::parse(connection_schema);
+  }
 
-    RandomJSONgenerator json_generator;
-    nlohmann::json connection_object_json;
-    nlohmann::json connection_schema_json;
-    /// TODO: declare other VDA5050 JSON objects here
+  RandomJSONgenerator json_generator;
+  nlohmann::json connection_object_json;
+  nlohmann::json connection_schema_json;
+  /// TODO: declare other VDA5050 JSON objects here
 };
 
 /// \brief tests that a valid VDA5050 JSON object passes against the schema validator
 TEST_F(JsonValidatorTest, BasicValidationTest)
 {
-    int connection_result = validate_schema(connection_schema_json, connection_object_json);
-    EXPECT_EQ(connection_result, 1);
+  int connection_result =
+    validate_schema(connection_schema_json, connection_object_json);
+  EXPECT_EQ(connection_result, 1);
 }
